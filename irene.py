536458f9751ffcc3dfde0f506f3747b0f4c64767
irenejimmy <irenejimmy2028@cs.sjcetpalai.ac.in>
2024-10-04 16:05:20 +0530

    Add files via upload

---
+++ irene.py
@@ -0,0 +1,60 @@
+
+print("hello \nworld")
+print("hello world")
+
+if 5>2:
+    print("5 is greater than 2")
+if 5 > 2:
+        print("5 is greater than 2")
+if 5 > 2:
+            print("5 is greater than 2")
+            
+if 5>2:
+    print("5 is greater than 2")
+    print("5 is greater than 2")
+
+
+x=1j
+y="thomas"
+print(type(x))
+print(type(y))
+
+x=str(3)
+y=int(3)
+z=float(3)
+print(x)
+print(y)
+print(z)
+
+myVar="john"
+print(myVar)
+
+
+x,y,z="orange","apple","cherry"
+print(x)
+print(y)
+print(z)
+
+x=y=z="orange"
+print(x)
+print(y)
+print(z)
+
+x="python"
+y=" is"
+z=" awesome"
+print(x + y + z)
+
+username=input("Enter username:")
+print("username is: " +username )
+
+a ="hello"
+b=" world"
+c=a+b
+print(c)
+
+a= "hello world"
+print(a[2])
+
+b="Hello World"
+print(b[2:5])
