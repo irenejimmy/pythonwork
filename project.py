2d7d50a3855ad36adbe8202979ad10597ff30ad3
irenejimmy <irenejimmy2028@cs.sjcetpalai.ac.in>
2024-10-18 15:06:54 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,8 @@
+'''
+Author:Irene Jimmy
+Date: 18-10-24
+Description:program that asks the user to input their name and age. After receiving the input, display a message that includes the user's name and age.
+'''
+name=input("Enter your name")
+age=input("enter your age")
+print("Hello,",name+"!you are",age,"years old")
