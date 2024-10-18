97975efe87fb8cf2963f88f860052002abf7d175
irenejimmy <irenejimmy2028@cs.sjcetpalai.ac.in>
2024-10-18 15:05:53 +0530

    Add files via upload

---
+++ basicstring.py
@@ -0,0 +1,11 @@
+'''
+Author: Irene Jimmy
+Date:18-10-24
+Description:program that stores a string in a variable. Extract a specific part of the string (substring) and then concatenate it with another string. Finally, display the new string.
+'''
+var1="Hello, World!"
+var2=var1[7:12]
+print(var2)
+var3=" Everyone!"
+var4=var2+var3
+print(var4)
