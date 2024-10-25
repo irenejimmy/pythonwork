7fe4b3b607ac032d3940e3c352bf73c5a58e279b
irenejimmy <irenejimmy2028@cs.sjcetpalai.ac.in>
2024-10-25 14:29:00 +0530

    Add files via upload

--- positive.py
+++ positive.py
@@ -1,3 +1,8 @@
+'''
+Author:Irene Jimmy
+date:25-10-2024
+description:a Python program to find whether a number is positive or negative.
+'''
 num1=int(input("Enter a number:"))
 if num1>0:
     print(num1,"is positive")
