c466574cae51a67e0e056cdc364b008bfcbb3973
irenejimmy <irenejimmy2028@cs.sjcetpalai.ac.in>
2024-10-25 16:08:35 +0530

    Add files via upload

---
+++ twinkle.py
@@ -0,0 +1,8 @@
+'''
+Author:Irene Jimmy
+date:25-10-2024
+description: Write a Python program to print the following string in a specific format (see the output).
+Sample String : "Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are"
+'''
+str="Twinkle, twinkle, little star,\n\tHow I wonder what you are!\n\t Up above the world so high,\t\t\nLike a diamond in the sky.\n\t Twinkle, twinkle, little star, How I wonder what you  are"
+print(str)
