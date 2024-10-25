0a50b9b9694093e150a8876bd103f64b30c0117c
irenejimmy <irenejimmy2028@cs.sjcetpalai.ac.in>
2024-10-25 14:30:48 +0530

    Add files via upload

--- largest.py
+++ largest.py
@@ -1,6 +1,10 @@
+'''
+Author:Irene Jimmy
+date:25-10-2024
+description:a Python program to find greater  number.
+'''
 num1=int(input("Enter first number:"))
 num2=int(input("Enter second number:"))
-#num3=int(input("Enter third number"))
 if num1>num2:
     print(num1,"is greater than",num2)
 else:
