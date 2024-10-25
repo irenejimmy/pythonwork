bc141af2538f8c1886c97c3a9b0b2e221ed3ec75
irenejimmy <irenejimmy2028@cs.sjcetpalai.ac.in>
2024-10-25 14:35:34 +0530

    Add files via upload

---
+++ positiveorzero.py
@@ -0,0 +1,12 @@
+'''
+Author:Irene Jimmy
+date:25-10-2024
+description:a Python program to find whether a number is positive or negative or zero.
+'''
+num1=int(input("Enter a number:"))
+if num1>0:
+    print(num1,"is positive")
+elif num1<0:
+    print(num1,"is negative")
+else:
+    print(num1,"is zero")
